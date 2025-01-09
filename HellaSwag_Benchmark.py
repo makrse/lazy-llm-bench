471a17c7569f7ffeeb548e8bc4466b60e92ca6c4
makrse <131285038+makrse@users.noreply.github.com>
2025-01-10 03:39:37 +0800

    Update HellaSwag_Benchmark.py
    Useful descriptions

--- HellaSwag_Benchmark.py
+++ HellaSwag_Benchmark.py
@@ -100,32 +100,33 @@ class GGUFModelLoader:
             n_gpu_layers=self.n_gpu_layers,
             use_flash_attention=self.use_flash_attention,
             use_fp16=self.use_fp16,
-            verbose=False
+            verbose=False # log stuffs on console. sometimes useful see if gpu works or not and other loading model data.
         )
     
+# Do not modify above if you dont know what it does.
 
 if __name__ == "__main__":
-    model_path = os.path.join("llama2_13b.gguf")
+    model_path = os.path.join("name_of_llm.gguf") # self descriptive.
     
     gguf_loader = GGUFModelLoader(
         model_path=model_path, 
-        context_length=4096,
-        temperature=0,
-        n_predict=-1, 
-        cpu_threads=4, 
-        n_gpu_layers=40, 
-        keep_model_in_ram=True, 
-        use_flash_attention=True,
-        use_fp16=True  # Enable FP16,
+        context_length=0, # Actual token lengh of the model you want to asign so the model memorize, mostly they tell you how much tokens, it could consume ram
+        temperature=0, # 0 rigid , more than "1" creative but allucinate.
+        n_predict=-1, # This does not take in count, depends, its for how many tokens you want to give to generate an answer, but since its a,b,c,d answers...
+        cpu_threads=0 # ***"how many cpu cores to give it", 
+        n_gpu_layers=0 # how many gpu layers/cores to give it 0-60?, give more , more fast response.
+        keep_model_in_ram=True, # Put or not the llm model into the ram, despite being false or true it will also load on the gpu, you could try false or true to see difference.
+        use_flash_attention=True, # Make fast , its the "thing"
+        use_fp16=True  # Enable FP16 , being said its for consumer gpus, if you had other type that allows bf16, you could tweak it, for me fp16 its fast over fp32, almost same acc.
     )
     model = gguf_loader.load()
 
-    benchmark = HellaSwag(
+    benchmark = HellaSwag( # check their task list, some are useful, some are not, like Health tast only has 4 entries, gives good score making not that "good" for overal score if rigid
         tasks=[HellaSwagTask.COMPUTERS_AND_ELECTRONICS, HellaSwagTask.INSTALLING_CARPET, HellaSwagTask.EDUCATION_AND_COMMUNICATIONS,
                HellaSwagTask.WORK_WORLD, HellaSwagTask.FINANCE_AND_BUSINESS, 
                ],
-        n_shots=0,
-        verbose_mode=True
+        n_shots=0, # being 0 called zero shots, they said default being 10, no more than 15.
+        verbose_mode=True # log stuffs on console.
     )
 
     # Evaluate the model on ARC
