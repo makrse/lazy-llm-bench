b3c25b781ab47a37bf10799221da814da1c72ba5
makrse <131285038+makrse@users.noreply.github.com>
2025-01-10 16:20:54 +0800

    Update MMLU_Benchmark.py

--- MMLU_Benchmark.py
+++ MMLU_Benchmark.py
@@ -107,24 +107,24 @@ class GGUFModelLoader:
 # You can modify below to test the code.
 
 if __name__ == "__main__":
-    model_path = os.path.join("llama2_13b.gguf")
+    model_path = os.path.join("name_of_llm.gguf") # self descriptive.
     
     gguf_loader = GGUFModelLoader(
         model_path=model_path, 
-        context_length=4096,
-        temperature=0,
-        n_predict=-1, 
-        cpu_threads=4, 
-        n_gpu_layers=40, 
-        keep_model_in_ram=True, 
-        use_flash_attention=True,
-        use_fp16=True  # Enable FP16,
+        context_length=0, # Actual token lengh of the model you want to asign so the model memorize, mostly they tell you how much tokens, it could consume ram
+        temperature=0, # 0 rigid , more than "1" creative but allucinate.
+        n_predict=-1, # This does not take in count, depends, its for how many tokens you want to give to generate an answer, but since its a,b,c,d answers...
+        cpu_threads=0 # ***"how many cpu cores to give it", 
+        n_gpu_layers=0 # how many gpu layers/cores to give it 0-60?, give more , more fast response.
+        keep_model_in_ram=True, # Put or not the llm model into the ram, despite being false or true it will also load on the gpu, you could try false or true to see difference.
+        use_flash_attention=True, # Make fast , its the "thing"
+        use_fp16=True  # Enable FP16 , being said its for consumer gpus, if you had other type that allows bf16, you could tweak it, for me fp16 its fast over fp32, almost same acc.
     )
     model = gguf_loader.load()
 
-    benchmark = MMLU(
+    benchmark = MMLU( # check their task list
         tasks=[MMLUTask.HIGH_SCHOOL_COMPUTER_SCIENCE, MMLUTask.ASTRONOMY],
-        n_shots=0
+        n_shots=0    # zero-shots | default 10, do not go above 15.
     )
 
     benchmark.evaluate(model)
